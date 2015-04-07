d91ba4d30be87abd66f8efef93acc5beab26ee1c
n0impossible <pangkiarifin@gmail.com>
2015-04-08 04:53:19 +0700

    link perpanjang

--- proleague_mailer_template.js
+++ proleague_mailer_template.js
@@ -2,13 +2,13 @@ exports.template = {
 	sevendays:"Halo,<br /><br />\
 			Akun proleague loe 7 hari akan berakhir<br /><br />\
 			Untuk dapat terus menikmati fasilitas proleague,<br />\
-			silahkan perpanjang akun loe <a href='http://fm.supersoccer.co.id/'>disini</a><br />\
+			silahkan perpanjang akun loe <a href='http://fm.supersoccer.co.id/upgrade/plan'>disini</a><br />\
 			Regards,<br /><br />\
 			FM SuperSoccer",
 	threedays:"Halo,<br /><br />\
 			Akun proleague loe 3 hari akan berakhir<br /><br />\
 			Untuk dapat terus menikmati fasilitas proleague,<br />\
-			silahkan perpanjang akun loe <a href='http://fm.supersoccer.co.id/'>disini</a><br />\
+			silahkan perpanjang akun loe <a href='http://fm.supersoccer.co.id/upgrade/plan'>disini</a><br />\
 			Regards,<br /><br />\
 			FM SuperSoccer"
 			};
